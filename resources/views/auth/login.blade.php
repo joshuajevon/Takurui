2e4a5dc12effbd40fc4e858ae03c0425eb426e8a
TessyJr <christopher.nathanael1217@gmail.com>
2023-11-16 18:41:49 +0700

    add: add login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,75 +1,101 @@
 @extends('template.template')
 
 @section('head')
-    {{-- css js --}}
+{{-- css js --}}
 
 @endsection
 
 @section('body')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="row p-0 m-0">
+    <div class="pt-5 d-none d-xl-flex col-lg-4 bg-secondary-subtle align-items-end">
+        <div class="pt-5">
+            <div class="pt-5">
+                <div class="pt-5">
+                    <img src="{{asset('assets/common/mascot.png')}}" alt="TakuRui mascot" class="object-contain w-100">
+                </div>
+            </div>
+        </div>
+    </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <div class="py-5 col-12 col-xl-8 d-flex align-items-center px-5">
+        <div class="py-5 w-100">
+            <div class="pt-5 w-100">
+                <div class="pt-5 d-flex flex-column w-100">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                    <div>
+                        <h1 class="fw-bold mb-4">
+                            {{ __('Login') }}
+                        </h1>
+                    </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="email" class="col-12 col-form-label fw-bold">{{ __('Email') }}</label>
 
-                                @error('email')
+                                <div class="col-12">
+                                    <input id="email" type="email" class="py-3 rounded-0 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Insert your email">
+
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                    @enderror
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <div class="row mb-3">
+                                <label for="password" class="col-12 col-form-label fw-bold">{{ __('Password') }}</label>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                <div class="col-12">
+                                    <input id="password" type="password" class="py-3 rounded-0 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Insert your password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+
+                                    @if (Route::has('password.request'))
+                                    <div class="d-flex justify-content-end mt-2">
+                                        <a class="text-decoration-none text-black text-end" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    </div>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="row mb-3">
+                                <div class="col-12">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-dark py-2 py-sm-3 px-4 px-sm-5 text-center text-light rounded-0">
+                                        {{ __('LOGIN') }}
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-12">
+                                    <p class="m-0">Don't have an account?
+                                        <a href="/register" class="fw-bold text-decoration-none text-black">Register here.</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/main/welcome.blade.php
+++ resources/views/main/welcome.blade.php
@@ -17,7 +17,7 @@
             <div class="carousel-item active">
                 <div class="d-flex flex-column gap-4 align-items-center justify-content-center bg-secondary vh-100">
                     <h1>Welcome to TakuRui</h1>
-                    <a href="" class="btn btn-light rounded-0 px-5 py-3">
+                    <a href="" class="btn btn-light py-2 py-sm-3 px-4 px-sm-5 text-center rounded-0">
                         <span class="fs-5">SHOP NOW</span>
                     </a>
                 </div>
@@ -46,8 +46,8 @@
 
 {{-- Our Latest Product --}}
 <div class="py-5">
-    <div class="container py-5 d-flex flex-column">
-        <h1 class="text-center display-3">BEST SELLERS</h1>
+    <div class="container py-5 d-flex flex-column align-items-center">
+        <h1 class="text-center display-3 w-100">BEST SELLERS</h1>
         @can('isAdmin')
         <a class="text-light text-decoration-none mt-3" href="/admin/product/create-product">
             <button type="button" class="btn btn-primary p-lg-2 px-lg-3 px-2 p-1 d-flex justify-content-center align-items-center gap-2 fw-semibold">
@@ -59,10 +59,10 @@
         </a>
         @endcan
 
-        <div class="row">
+        <div class="row w-100">
             {{-- Show 4 produk terbaru --}}
             @foreach ($products as $product)
-            <div href="" class="col-lg-3 col-6 pt-4">
+            <div class="col-lg-3 col-6 pt-4">
                 <div class="d-flex flex-column gap-2 gap-lg-3 w-100">
                     <div class="w-100" style="aspect-ratio: 3/4;">
                         {{-- <img src="{{ asset('assets/temp/sample-2.png')}}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}"> --}}
@@ -79,8 +79,11 @@
                         <a href="{{ route('productById', $product->id) }}" class="rounded-0 btn btn-outline-dark p-lg-2 p-1 w-100">View</a>
                         @guest
                         <!-- Button trigger modal -->
-                        <a href="{{ route('register') }}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="rounded-0 btn btn-dark text-light p-lg-2 p-1">
-                            Add to Cart
+                        <a href="{{ route('register') }}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="rounded-0 btn btn-dark rounded text-light py-lg-2 py-1 px-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                            </svg>
                         </a>
 
                         <!-- Modal -->
@@ -121,7 +124,25 @@
             @endforeach
         </div>
 
-        <a href="/products" class="d-inline btn btn-dark py-3 text-center text-light mt-4 rounded-0">SHOP NOW</a>
+        <a href="/products" class="btn btn-dark py-2 py-sm-3 px-4 px-sm-5 text-center text-light mt-4 rounded-0" style="width: fit-content">SHOP ALL</a>
+    </div>
+</div>
+
+{{-- Annoucement --}}
+<div class="container pb-5">
+    <div class="row">
+        <div class="mt-4 mt-sm-0 col-12 col-sm-6 order-2 order-sm-1">
+            <div class="w-100 h-100 d-flex flex-column justify-content-center">
+                <h1>The Forger's Are Back!</h1>
+                <p>"Waku Waku."Anya's favorite things include peanuts, spy cartoons, cake, castles, penguins, her uniform, Mama, Papa, and Becky ♥</p>
+
+                <a href="/products" class="btn btn-dark py-2 py-sm-3 px-4 px-sm-5 text-center text-light rounded-0" style="width: fit-content">SHOP NOW</a>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 order-1 order-sm-2">
+            <img src="{{asset('assets/home-page/spyxfamily.jpg')}}" alt="spy x family" class="w-100">
+        </div>
     </div>
 </div>
 
