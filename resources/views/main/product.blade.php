18fd7ed3250b2ade6b1f07ea2ecd8d286826cc71
TessyJr <christopher.nathanael1217@gmail.com>
2023-11-19 20:04:45 +0700

    style: fix home and show now stylings

--- resources/views/main/product.blade.php
+++ resources/views/main/product.blade.php
@@ -1,147 +1,176 @@
 @extends('template.template')
 
 @section('head')
-    {{-- css js --}}
-    <style>
+{{-- css js --}}
+<style>
+    .card-img-top {
+        height: 250px;
+        widows: 100%;
+    }
+
+    .text-currency {
+        font-size: 1.75rem;
+    }
+
+    @media (max-width: 991px) {
         .card-img-top {
-            height: 250px;
-            widows: 100%;
-        }
-        .text-currency{
-            font-size: 1.75rem;
+            max-width: 250px;
+            width: 50%;
+            max-height: 250px;
+            height: auto;
         }
 
-        @media (max-width: 991px) {
-            .card-img-top {
-                max-width: 250px;
-                width: 50%;
-                max-height: 250px;
-                height: auto;
-            }
-            .text-currency{
-                font-size: 1.25rem;
-            }
+        .text-currency {
+            font-size: 1.25rem;
         }
-    </style>
+    }
+
+</style>
 @endsection
 
 @section('body')
-
-
-<br>
-<br><br><br><br><br><br><br>
-
 {{-- All Product --}}
-<div class="container d-flex flex-column gap-5 min-vh-100">
-    <h1 class="text-center fw-bolder display-4"><span class="border-bottom border-dark border-5">Our
-            Products</span></h1>
+<div class="pt-5">
+    <div class="pt-5">
+        <div class="pt-5">
+            <div class="container d-flex flex-column align-items-center gap-4 gap-lg-5 min-vh-100 py-5">
+                <h1 class="text-center display-3 w-100">OUR PRODUCTS</h1>
 
-    @if ($products->count() == 0)
-        <div class="">
-            <h1>Maaf, hasil pencarian produk dengan kata kunci "{{ $result }}" belum tersedia. <a
-                    href="/" class="underline text-blue-500">Klik disini</a> untuk kembali ke
-                Beranda
-            </h1>
-        </div>
-    @else
-        @if ($result)
-            <div class="">
-                <h1>Hasil pencarian produk dengan kata kunci "{{ $result }}"</h1>
-            </div>
-        @endif
-    @endif
+                @if ($products->count() == 0)
+                <div class="">
+                    <h1>Maaf, hasil pencarian produk dengan kata kunci "{{ $result }}" belum tersedia. <a href="/" class="underline text-blue-500">Klik disini</a> untuk kembali ke
+                        Beranda
+                    </h1>
+                </div>
+                @else
+                @if ($result)
+                <div class="">
+                    <h1>Hasil pencarian produk dengan kata kunci "{{ $result }}"</h1>
+                </div>
+                @endif
+                @endif
 
-    @can('isAdmin')
-    <a class="text-light text-decoration-none" href="/admin/product/create-product">
-            <button type="button"
-                class="btn btn-primary p-lg-2 px-lg-3 px-2 p-1 d-flex justify-content-center align-items-center gap-2 fw-semibold">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                    class="bi bi-plus-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                    <path
-                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                </svg>Add Item
-            </button>
-        <
-    </a>
-    @endcan
+                @can('isAdmin')
+                <a class="text-light text-decoration-none" href="/admin/product/create-product">
+                    <button type="button" class="btn btn-primary p-lg-2 px-lg-3 px-2 p-1 d-flex justify-content-center align-items-center gap-2 fw-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
+                        Add Item
+                    </button>
+                </a>
+                @endcan
 
-    {{-- Filter --}}
-    <a href="{{route('filterCat', 1)}}">Limited Edition</a>
-    <a href="{{route('filterCat',2)}}">Standard</a>
-    
+                <div class="d-flex flex-column gap-5 align-items-center w-100">
+                    <div class="d-flex flex-column w-100">
+                        <h5>Filter</h5>
+                        <div class="w-100">
+                            <a href="{{ route('filterCat', 1) }}" class="btn {{ request()->is('products/1*') ? 'btn-dark' : 'btn-outline-dark' }}">Limited Edition</a>
 
-    <a href="{{route('product')}}">Reset</a>
-        {{-- product --}}
-        <div class="row">
-            @foreach ($products as $product)
-            <div class="col-lg-4 col-12 p-3">
-                <div
-                    class="border p-lg-4 p-3 rounded border-dark d-flex align-items-center flex-column flex-row gap-lg-4 gap-2">
-                    <h2 class="text-truncate d-inline-block d-lg-none w-100 text-center">Product 1</h2>
-                    <div class="d-flex align-items-center flex-lg-column flex-row gap-lg-4 gap-1 w-100">
-                        <img src="{{asset('/storage/image/'.$product->image)}}" class="object-fit-contain rounded card-img-top" alt="product">
-                        <div class="d-flex flex-column gap-lg-2 gap-1 ps-lg-0 ps-3 w-100">
-                            <h2 class="text-truncate d-lg-inline-block d-none">{{$product->name}}</h2>
-                            <p class="lead d-lg-block d-none overflow-scroll overflow-x-hidden" style="height: 120px">
-                                {{$product->description}}</p>
-                            <h5><span
-                                    class="badge bg-secondary text-light">{{$product->category->CategoryName}}</span>
-                            </h5>
-                            <h3 class="text-currency">@currency ($product->price)</h3>
-                            <a href="{{route('productById', $product->id)}}" class="btn btn-outline-dark fw-semibold p-lg-2 p-1">View</a>
-                            @guest
+                            <a href="{{ route('filterCat', 2) }}" class="btn {{ request()->is('products/2*') ? 'btn-dark' : 'btn-outline-dark' }}">Standard</a>
 
-                            <!-- Button trigger modal -->
-
-                            <a href="{{ route('register') }}" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                                class="btn btn-dark text-light fw-semibold p-lg-2 p-1">
-                                Add To Cart
-                            </a>
-
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                                aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Warning</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            Please Register / Login before Add The Product to Cart!
-                                        </div>
-                                        <div class="modal-footer">
-                                            <a href="{{ route('register') }}"
-                                                class="btn btn-success py-lg-2 rounded text-center text-light fw-semibold">
-                                                Register
-                                            </a>
-                                            <a href="{{ route('login') }}"
-                                                class="btn btn-primary py-lg-2 rounded text-center text-light fw-semibold">
-                                                Login
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            @endguest
-
-                            @auth
-                            <a href="{{ route('addToCart', $product->id) }}"
-                                class="btn btn-dark text-light fw-semibold p-lg-2 p-1">Add To
-                                Cart</a>
-                            @endauth
-
-
+                            <a href="{{ route('product') }}" class="btn btn-danger">Reset</a>
                         </div>
                     </div>
-                </div>
-            </div>
-            @endforeach
-        </div>
-        {{ $products->links() }}
 
+                    {{-- product --}}
+                    <div class="row gy-4 gy-lg-5 g-3 g-lg-4 w-100">
+                        {{-- Show 4 produk terbaru --}}
+                        @foreach ($products as $product)
+                        <div class="col-lg-3 col-6">
+                            <a href="{{ route('productById', $product->id) }}" class="text-decoration-none text-black">
+                                <div class="d-flex flex-column gap-2 gap-lg-3 w-100">
+                                    <div class="w-100" style="aspect-ratio: 3/4;">
+                                        <img src="{{ asset('assets/temp/sample-2.png')}}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}">
+
+                                        {{-- <img src="{{ asset('/storage/image/' . $product->image) }}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}"> --}}
+                                    </div>
+
+                                    <div class="d-flex flex-column gap-1 gap-lg-2 w-100">
+                                        <span class="fs-5 fs-lg-4 text-truncate fw-medium">{{ $product->name }}</span>
+                                        <span class="fs-6 fs-lg-5 text-break">@currency ($product->price)</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        @endforeach
+                    </div>
+
+                    {{-- PAGIANTION --}}
+                    @if ($products->hasPages())
+                    <nav class="d-flex justify-items-center justify-content-between w-100">
+                        <div class="d-flex justify-content-between flex-fill d-sm-none">
+                            <ul class="pagination">
+                                {{-- Previous Page Link --}}
+                                @if ($products->onFirstPage())
+                                <li class="page-item disabled" aria-disabled="true">
+                                    <span class="page-link">@lang('pagination.previous')</span>
+                                </li>
+                                @else
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $products->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a>
+                                </li>
+                                @endif
+
+                                {{-- Next Page Link --}}
+                                @if ($products->hasMorePages())
+                                <li class="page-item">
+                                    <a class="page-link" href="{{ $products->nextPageUrl() }}" rel="next">@lang('pagination.next')</a>
+                                </li>
+                                @else
+                                <li class="page-item disabled" aria-disabled="true">
+                                    <span class="page-link">@lang('pagination.next')</span>
+                                </li>
+                                @endif
+                            </ul>
+                        </div>
+
+                        <div class="d-none flex-sm-fill d-sm-flex align-items-sm-center justify-content-sm-between">
+                            <div>
+                                <p class="small text-muted">
+                                    {!! __('Showing') !!}
+                                    <span class="fw-semibold">{{ $products->firstItem() }}</span>
+                                    {!! __('to') !!}
+                                    <span class="fw-semibold">{{ $products->lastItem() }}</span>
+                                    {!! __('of') !!}
+                                    <span class="fw-semibold">{{ $products->total() }}</span>
+                                    {!! __('results') !!}
+                                </p>
+                            </div>
+
+                            <div>
+                                <ul class="pagination">
+                                    {{-- Previous Page Link --}}
+                                    @if ($products->onFirstPage())
+                                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                                        <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                                    </li>
+                                    @else
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $products->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                                    </li>
+                                    @endif
+
+                                    {{-- Next Page Link --}}
+                                    @if ($products->hasMorePages())
+                                    <li class="page-item">
+                                        <a class="page-link" href="{{ $products->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                                    </li>
+                                    @else
+                                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                                        <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                                    </li>
+                                    @endif
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                    @endif
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- resources/views/main/welcome.blade.php
+++ resources/views/main/welcome.blade.php
@@ -44,8 +44,8 @@
     </div>
 </div>
 
-{{-- Our Latest Product --}}
-<div class="py-5">
+{{-- Latest Products --}}
+<div class="pt-5 pb-lg-5">
     <div class="container py-5 d-flex flex-column align-items-center">
         <h1 class="text-center display-3 w-100">LATEST PRODUCTS</h1>
         @can('isAdmin')
@@ -59,140 +59,54 @@
         </a>
         @endcan
 
-        <div class="row w-100">
+        <div class="row gy-4 gy-lg-5 g-3 g-lg-4 w-100 pt-4">
             {{-- Show 4 produk terbaru --}}
             @foreach ($products as $product)
-            <div class="col-lg-3 col-6 pt-4">
-                <div class="d-flex flex-column gap-2 gap-lg-3 w-100">
-                    <div class="w-100" style="aspect-ratio: 3/4;">
-                        {{-- <img src="{{ asset('assets/temp/sample-2.png')}}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}"> --}}
+            <div class="col-lg-3 col-6">
+                <a href="{{ route('productById', $product->id) }}" class="text-decoration-none text-black">
+                    <div class="d-flex flex-column gap-2 gap-lg-3 w-100">
+                        <div class="w-100" style="aspect-ratio: 3/4;">
+                            {{-- <img src="{{ asset('assets/temp/sample-2.png')}}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}"> --}}
 
-                        <img src="{{ asset('/storage/image/' . $product->image) }}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}">
-                    </div>
-
-                    <div class="d-flex flex-column gap-1 gap-lg-2 w-100">
-                        <span class="fs-5 fs-lg-4 text-truncate fw-medium">{{ $product->name }}</span>
-                        <span class="fs-6 fs-lg-5 text-break">@currency ($product->price)</span>
-                    </div>
-
-                    <div class="d-flex gap-2 w-100">
-                        <a href="{{ route('productById', $product->id) }}" class="rounded-0 btn btn-outline-dark p-lg-2 p-1 w-100">View</a>
-                        @guest
-                        <!-- Button trigger modal -->
-                        <a href="{{ route('register') }}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="rounded-0 btn btn-dark rounded text-light py-lg-2 py-1 px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                            </svg>
-                        </a>
-
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Warning</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Please Register / Login before Add The Product to Cart!
-                                    </div>
-                                    <div class="modal-footer">
-                                        <a href="{{ route('register') }}" class="btn btn-success py-lg-2 rounded text-light fw-semibold">
-                                            Register
-                                        </a>
-                                        <a href="{{ route('login') }}" class="btn btn-primary py-lg-2 rounded text-light fw-semibold">
-                                            Login
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                            <img src="{{ asset('/storage/image/' . $product->image) }}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}">
                         </div>
-                        @endguest
 
-                        @auth
-                        <a href="{{ route('addToCart', $product->id) }}" class="rounded-0 btn btn-dark rounded text-light py-lg-2 py-1 px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                            </svg>
-                        </a>
-                        @endauth
+                        <div class="d-flex flex-column gap-1 gap-lg-2 w-100">
+                            <span class="fs-5 fs-lg-4 text-truncate fw-medium">{{ $product->name }}</span>
+                            <span class="fs-6 fs-lg-5 text-break">@currency ($product->price)</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
             @endforeach
         </div>
 
-        <a href="/products" class="btn btn-dark py-2 py-sm-3 px-4 px-sm-5 text-center text-light mt-4 rounded-0" style="width: fit-content">SHOP ALL</a>
+        <a href="/products" class="btn btn-dark py-2 py-sm-3 px-4 px-sm-5 text-center text-light mt-4 mt-lg-5 rounded-0" style="width: fit-content">SHOP ALL</a>
     </div>
 </div>
 
-{{-- Limited Edition --}}
-<div class="py-5">
+{{-- Limited Products --}}
+<div class="py-lg-5">
     <div class="container py-5 d-flex flex-column align-items-center">
-        <h1 class="text-center display-3 w-100">LIMITED EDITION</h1>
+        <h1 class="text-center display-3 w-100">LIMITED PRODUCTS</h1>
 
-        <div class="row w-100">
+        <div class="row gy-4 gy-lg-5 g-3 g-lg-4 w-100 pt-4">
             @foreach ($limitedEdition as $product)
-            <div class="col-lg-3 col-6 pt-4">
-                <div class="d-flex flex-column gap-2 gap-lg-3 w-100">
-                    <div class="w-100" style="aspect-ratio: 3/4;">
-                        {{-- <img src="{{ asset('assets/temp/sample-2.png')}}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}"> --}}
+            <div class="col-lg-3 col-6">
+                <a href="{{ route('productById', $product->id) }}" class="text-decoration-none text-black">
+                    <div class="d-flex flex-column gap-2 gap-lg-3 w-100">
+                        <div class="w-100" style="aspect-ratio: 3/4;">
+                            {{-- <img src="{{ asset('assets/temp/sample-2.png')}}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}"> --}}
 
-                        <img src="{{ asset('/storage/image/' . $product->image) }}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}">
-                    </div>
-
-                    <div class="d-flex flex-column gap-1 gap-lg-2 w-100">
-                        <span class="fs-5 fs-lg-4 text-truncate fw-medium">{{ $product->name }}</span>
-                        <span class="fs-6 fs-lg-5 text-break">@currency ($product->price)</span>
-                    </div>
-
-                    <div class="d-flex gap-2 w-100">
-                        <a href="{{ route('productById', $product->id) }}" class="rounded-0 btn btn-outline-dark p-lg-2 p-1 w-100">View</a>
-                        @guest
-                        <!-- Button trigger modal -->
-                        <a href="{{ route('register') }}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="rounded-0 btn btn-dark rounded text-light py-lg-2 py-1 px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                            </svg>
-                        </a>
-
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Warning</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Please Register / Login before Add The Product to Cart!
-                                    </div>
-                                    <div class="modal-footer">
-                                        <a href="{{ route('register') }}" class="btn btn-success py-lg-2 rounded text-light fw-semibold">
-                                            Register
-                                        </a>
-                                        <a href="{{ route('login') }}" class="btn btn-primary py-lg-2 rounded text-light fw-semibold">
-                                            Login
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                            <img src="{{ asset('/storage/image/' . $product->image) }}" class="object-fit-cover w-100 h-100" alt="{{ $product->name }}">
                         </div>
-                        @endguest
 
-                        @auth
-                        <a href="{{ route('addToCart', $product->id) }}" class="rounded-0 btn btn-dark rounded text-light py-lg-2 py-1 px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                            </svg>
-                        </a>
-                        @endauth
+                        <div class="d-flex flex-column gap-1 gap-lg-2 w-100">
+                            <span class="fs-5 fs-lg-4 text-truncate fw-medium">{{ $product->name }}</span>
+                            <span class="fs-6 fs-lg-5 text-break">@currency ($product->price)</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
             @endforeach
         </div>
@@ -202,7 +116,7 @@
 </div>
 
 {{-- Annoucement --}}
-<div class="container pb-5">
+<div class="container py-5">
     <div class="row">
         <div class="mt-4 mt-sm-0 col-12 col-sm-6 order-2 order-sm-1">
             <div class="w-100 h-100 d-flex flex-column justify-content-center">
